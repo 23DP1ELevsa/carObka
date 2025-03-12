9a99934bb4c8718cd3878ad2ed769ad0a83af5be
Eduards Levša <166118110+23DP1ELevsa@users.noreply.github.com>
2025-03-12 08:56:24 +0000

    login method

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -1,16 +1,15 @@
 package lv.rvt;
 
-import java.util.*;
-
 import lv.rvt.tools.Login;
 
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("Hello World!");
-        System.out.println("Eddy ready");
-        Login login = new Login("alo", "hacha");
-        login.getUsername();
+public class Main {
+    public static void main(String[] args) {
+        Login login = new Login("", "");
+        
+        String enteredUsername = login.getUsername();
+        String enteredPassword = login.getPassword();
+        
+        System.out.println("\nSveiki, " + enteredUsername + "!");
+        System.out.println("Tava parole: " + enteredPassword);
     }
 }
--- src/main/java/lv/rvt/tools/Login.java
+++ src/main/java/lv/rvt/tools/Login.java
@@ -5,30 +5,24 @@ import java.util.Scanner;
 public class Login extends LoginRegister {
     private String username;
     private String password;
+    private Scanner scanner = new Scanner(System.in);
 
-    public Login(String username, String password){
+    public Login(String username, String password) {
         this.username = username;
         this.password = password;
-    };
-    Scanner scanner = new Scanner(System.in);
+    }
+
     @Override
     public String getUsername() {
-        System.out.print("Username: ");
-        this.username = String.valueOf(scanner.nextLine());
-        return this.username;
-    };
-    
-    @Override
-    public String getPassword() {
-        System.out.print("Password: ");
-        this.password = String.valueOf(scanner.nextLine());
-        return this.password;
-    };
-    
-    public String returnUsername() {
+        System.out.print("Ievadiet username: ");
+        this.username = scanner.nextLine();
         return this.username;
     }
-    public String returnPassword() {
+
+    @Override
+    public String getPassword() {
+        System.out.print("Ievadiet password: ");
+        this.password = scanner.nextLine();
         return this.password;
     }
 }
