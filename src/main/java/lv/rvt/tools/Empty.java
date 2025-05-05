6b54b14d41a1a2debe2155a873eb1cd7339c472a
Deniss Bukovskis <166384453+23DP1DBuko@users.noreply.github.com>
2025-05-05 20:30:21 +0000

    empty fix for cmd

--- src/main/java/lv/rvt/tools/Empty.java
+++ src/main/java/lv/rvt/tools/Empty.java
@@ -2,22 +2,19 @@ package lv.rvt.tools;
 
 public class Empty {
     public static void EmptyScreen() {
-        System.out.println(ConsoleColors.BLACK + "        ⠀⠀⠀⠀⠀⢀⣀⣀⣀⠀");
-        System.out.println(ConsoleColors.BLACK + "        ⠀⠀⠀⠰⡿⠿⠛⠛⠻⠿⣷⠀");
-        System.out.println(ConsoleColors.BLACK + "        ⠀⠀⠀⠀⠀⠀⣀⣄⡀⠀⠀⠀⠀⢀⣀⣀⣤⣄⣀⡀⠀");
-        System.out.println(ConsoleColors.BLACK + "        ⠀⠀⠀⠀⠀⢸⣿⣿⣷⠀⠀⠀⠀⠛⠛⣿⣿⣿⡛⠿⠷⠀");
-        System.out.println(ConsoleColors.BLACK + "⠀        ⠀⠀⠀⠀⠘⠿⠿⠋⠀⠀⠀⠀⠀⠀⣿⣿⣿⠇⠀");
-        System.out.println(ConsoleColors.BLACK + "⠀        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁");
-        System.out.println("");
-        System.out.println(ConsoleColors.YELLOW + "⠀⠀        ⠀⠀⣿⣷⣄" + ConsoleColors.BLACK + "⠀⢶⣶⣷⣶⣶⣤⣀⠀");
-        System.out.println(ConsoleColors.YELLOW + "⠀⠀        ⠀⠀⣿⣿⣿⠀⠀⠀" + ConsoleColors.BLACK + "⠀⠀⠈⠙⠻⠗⠀");
-        System.out.println(ConsoleColors.YELLOW + "⠀⠀        ⠀⣰⣿⣿⣿⠀⠀⠀⠀⢀⣀⣠⣤⣴⣶⡄");
-        System.out.println(ConsoleColors.YELLOW + "⠀        ⣠⣾⣿⣿⣿⣥⣶⣶⣿⣿⣿⣿⣿⠿⠿⠛⠃");
-        System.out.println(ConsoleColors.YELLOW + "        ⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄");
-        System.out.println(ConsoleColors.YELLOW + "        ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁");
-        System.out.println(ConsoleColors.YELLOW + "        ⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁");
-        System.out.println(ConsoleColors.YELLOW + "⠀        ⠀⠛⢿⣿⣿⣿⣿⣿⣿⡿⠟");
-        System.out.println(ConsoleColors.YELLOW + "⠀⠀        ⠀⠀⠀⠉⠉⠉");
+        System.out.println("▒▒▒▒▒▒▒▒"+ConsoleColors.YELLOW+"▄▄▄▄▄▄▄▄"+ConsoleColors.RESET+"▒▒▒▒▒▒▒▒");
+        System.out.println("▒▒▒▒▒"+ConsoleColors.YELLOW+"▄█▀▀"+ConsoleColors.RESET+"░░░░░░"+ConsoleColors.YELLOW+"▀▀█▄"+ConsoleColors.RESET+"▒▒▒▒▒");
+        System.out.println("▒▒▒"+ConsoleColors.YELLOW+"▄█▀▄██▄"+ConsoleColors.RESET+"░░░░░░░░"+ConsoleColors.YELLOW+"▀█▄"+ConsoleColors.RESET+"▒▒▒");
+        System.out.println("▒▒"+ConsoleColors.YELLOW+"█▀"+ConsoleColors.RESET+"░"+ConsoleColors.YELLOW+"▀"+ConsoleColors.RESET+"░░"+ConsoleColors.YELLOW+"▄▀"+ConsoleColors.RESET+"░░░░"+ConsoleColors.YELLOW+"▄▀▀▀▀"+ConsoleColors.RESET+"░"+ConsoleColors.YELLOW+"▀█"+ConsoleColors.RESET+"▒▒");
+        System.out.println("▒"+ConsoleColors.YELLOW+"█▀"+ConsoleColors.RESET+"░░░░"+ConsoleColors.YELLOW+"███"+ConsoleColors.RESET+"░░░░"+ConsoleColors.YELLOW+"▄█▄"+ConsoleColors.RESET+"░░░░"+ConsoleColors.YELLOW+"▀█"+ConsoleColors.RESET+"▒");
+        System.out.println("▒"+ConsoleColors.YELLOW+"█"+ConsoleColors.RESET+"░░░░░░"+ConsoleColors.YELLOW+"▀"+ConsoleColors.RESET+"░░░░░"+ConsoleColors.YELLOW+"▀█▀"+ConsoleColors.RESET+"░░░░░"+ConsoleColors.YELLOW+"█"+ConsoleColors.RESET+"▒");
+        System.out.println("▒"+ConsoleColors.YELLOW+"█"+ConsoleColors.RESET+"░░░░░░░░░░░░░░░░░░░░"+ConsoleColors.YELLOW+"█"+ConsoleColors.RESET+"▒");
+        System.out.println("▒"+ConsoleColors.YELLOW+"█"+ConsoleColors.RESET+"░░"+ConsoleColors.YELLOW+"██▄"+ConsoleColors.RESET+"░░"+ConsoleColors.YELLOW+"▀▀▀▀▄▄"+ConsoleColors.RESET+"░░░░░░░"+ConsoleColors.YELLOW+"█"+ConsoleColors.RESET+"▒");
+        System.out.println("▒"+ConsoleColors.YELLOW+"▀█"+ConsoleColors.RESET+"░"+ConsoleColors.YELLOW+"█"+ConsoleColors.RESET+"░"+ConsoleColors.YELLOW+"█"+ConsoleColors.RESET+"░░░"+ConsoleColors.YELLOW+"▄▄▄▄▄"+ConsoleColors.RESET+"░░░░░░"+ConsoleColors.YELLOW+"█▀"+ConsoleColors.RESET+"▒");
+        System.out.println("▒▒"+ConsoleColors.YELLOW+"▀█▀"+ConsoleColors.RESET+"░"+ConsoleColors.YELLOW+"▀▀▀▀"+ConsoleColors.RESET+"░"+ConsoleColors.YELLOW+"▄▄▄▀"+ConsoleColors.RESET+"░░░░"+ConsoleColors.YELLOW+"▄█▀"+ConsoleColors.RESET+"▒▒");
+        System.out.println("▒▒▒"+ConsoleColors.YELLOW+"█"+ConsoleColors.RESET+"░░░░░░"+ConsoleColors.YELLOW+"▀█"+ConsoleColors.RESET+"░░░░░"+ConsoleColors.YELLOW+"▄█▀"+ConsoleColors.RESET+"▒▒▒▒");
+        System.out.println("▒▒▒"+ConsoleColors.YELLOW+"█▄"+ConsoleColors.RESET+"░░░░░"+ConsoleColors.YELLOW+"▀█▄▄▄█▀▀"+ConsoleColors.RESET+"▒▒▒▒▒▒");
+        System.out.println("▒▒▒▒"+ConsoleColors.YELLOW+"▀▀▀▀▀▀▀"+ConsoleColors.RESET+"▒▒▒▒▒▒▒▒▒▒▒▒▒");
         System.out.println(ConsoleColors.RESET);
         System.out.println(ConsoleColors.YELLOW + "Hmm, izskatas, ka šī sadaļa ir tukša!");
     }
